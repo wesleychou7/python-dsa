7803091741cc1483aa68bb916662df21b01d1ce6
Wesley Chou <wesleychou9851@gmail.com>
2023-01-29 20:20:54 -0800

    attempt #1

---
+++ trees/tree-level-order.py
@@ -0,0 +1,60 @@
+class Node:
+
+    def __init__(self, val=None):
+
+        self.left = None
+        self.right = None
+        self.val =  val 
+
+
+def nextLevelTrees(curr_level_trees):
+    """
+    Print all values of the trees in curr_level_trees
+    Return a list of all child trees of all trees in curr_level_trees
+    """
+
+    next_level_trees = []
+
+    for tree in curr_level_trees:
+
+        print(tree.val, end=" ")
+
+        if tree.left:
+            next_level_trees.append(tree.left)
+        if tree.right:
+            next_level_trees.append(tree.right)
+
+    return next_level_trees
+    
+
+def levelOrderPrint(tree):
+    """
+    Given a binary tree of integers, print it in level order. The output will 
+    contain space between the numbers in the same level, and new line between 
+    different levels. 
+    """
+    # first tree (or first node)
+    curr_level_trees = nextLevelTrees([tree])
+
+    while curr_level_trees != []:
+        curr_level_trees = nextLevelTrees(curr_level_trees)
+
+
+
+t = Node(1)
+
+t.left = Node(2)
+t.right = Node(3)
+
+t.left.left = Node(4)
+t.right.left = Node(5)
+t.right.right = Node(6)
+
+levelOrderPrint(t)
+
+    
+
+    
+
+
+
